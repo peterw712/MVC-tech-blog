c5b456abcaa780c2b8007a1a585f07349912c045
Peter Wang <65141087+peterw712@users.noreply.github.com>
2021-06-08 17:09:53 -0400

    routes and authentication

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// checks if user is logged in and redirects to login page if they are not
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
