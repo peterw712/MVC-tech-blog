55d455b5ca17ea57b692542fe8f53b77a3162783
Peter Wang <65141087+peterw712@users.noreply.github.com>
2021-06-09 13:54:19 -0400

    fixed bug

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({
+    helpers
+});
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
