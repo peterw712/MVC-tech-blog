be187255b95eab16b410e4063899349dd65ba1a5
Peter Wang <65141087+peterw712@users.noreply.github.com>
2021-06-09 14:54:41 -0400

    dashboard view and crud operations

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,11 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/api', apiRoutes);
-router.use('/dashboard', dashboardRoutes);
-router.use('/', homeRoutes);
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes.js');
 
-router.use((req, res) => {
-    res.status(404).end();
-});
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes)
 
 module.exports = router;
